3752fee694003f526fb68500092d931c02cae1aa
4rcan31 <rodrigo.francoescobar123@gmail.com>
2024-03-11 01:42:45 -0600

    Add index page

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,9 +1,59 @@
 <?php
-layouts();
-inithtml();
-
+layout("principal/head");
+layout("principal/headerBar");
+layout("principal/scripts");
+layout("principal/banner");
+layout("store/sectionHeading");
+layout("store/product");
+layout("store/features");
+layout("store/cards");
+layout("store/footer");
 ?>
 
 
 
-<h1> Esto es el home</h1>
+<!DOCTYPE html>
+<html>
+<?php headPrincipal("home") ?>
+
+<body>
+    <?php headerBarPrincipal("home") ?>
+
+    <?php bannerPrincipal() ?>
+
+
+
+    <!-- Aca se tienen que llamar los ultimos 6 productos publicados nada mas -->
+    <div class="latest-products">
+        <div class="container">
+            <div class="row">
+
+                <?php sectionHeading("Ultimos productos", "Ver todos los productos", route("/products", false)) ?>
+
+                <?php
+                product("Audifonos", "https://upload.wikimedia.org/wikipedia/commons/3/3f/Rojos.png", "Son audifnos", "45.5");
+                product("hahkasjkasj", "https://upload.wikimedia.org/wikipedia/commons/3/3f/Rojos.png", "Son audifnos", "45.5");
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="best-features">
+        <div class="container">
+            <div class="row">
+                <?php sectionHeading("Fectures") ?>
+                <?php features(); ?>
+            </div>
+        </div>
+    </div>
+    
+
+    <?Php cardmd12("Titulo card", "Description", "Boton") ?>
+    <?php footerStore(); ?>
+
+    <?Php scriptsPrincipal() ?>
+</body>
+
+</html>

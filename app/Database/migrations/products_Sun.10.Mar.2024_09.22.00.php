22fa829d59e17186c2931412ac9455b701846255
4rcan31 <rodrigo.francoescobar123@gmail.com>
2024-03-14 02:58:35 -0600

    Update Table products, add description large and short en url img and stock

--- app/Database/migrations/products_Sun.10.Mar.2024_09.22.00.php
+++ app/Database/migrations/products_Sun.10.Mar.2024_09.22.00.php
@@ -7,20 +7,24 @@ class products extends Migration {
             $this->query('CREATE TABLE products (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 name VARCHAR(255) NOT NULL,
-                description TEXT,
+                description_large TEXT,
+                description_short TEXT,
+                url_img TEXT,
+                stock INT DEFAULT 1,
                 price DECIMAL(10, 2) NOT NULL,
                 category_id INT,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (category_id) REFERENCES categories(id)
             )');
 
-            // Agregar 5 productos para cada categoría
             for ($i = 1; $i <= 4; $i++) { // suponiendo que las categorías tienen ids del 1 al 4
                 for ($j = 1; $j <= 5; $j++) {
                     $this->prepare();
                     $this->insert('products')->values([
-                        'name' => "Producto $j de la categoría $i",
-                        'description' => "Descripción del producto $j",
+                        'name' => "Nombre",
+                        'description_large' => "Descripción larga del producto $j",
+                        'description_short' => "Descripción Corta del producto $j",
+                        'url_img' => 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Rojos.png',
                         'price' => rand(10, 100), // Precio aleatorio entre 10 y 100
                         'category_id' => $i,
                     ]);

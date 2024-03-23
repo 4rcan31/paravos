d486f3c67b05caf6f316b722e105016c00c426c0
4rcan31 <rodrigo.francoescobar123@gmail.com>
2024-03-23 00:17:15 -0600

    Add user id reference, and insert test phone

--- app/Database/migrations/phones_Sun.10.Mar.2024_09.22.49.php
+++ app/Database/migrations/phones_Sun.10.Mar.2024_09.22.49.php
@@ -6,13 +6,21 @@ class phones extends Migration {
         $this->create("phones", function($table) {
             $this->query('CREATE TABLE phones (
                 id INT PRIMARY KEY AUTO_INCREMENT,
+                user_id INT,
+                is_principal BOOLEAN DEFAULT FALSE,
                 number_phone VARCHAR(255) NOT NULL,
-                user_id INT, -- Podría ser nulo si es un cliente registrado
-                client_id INT, -- Podría ser nulo si es un cliente no registrado
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL, -- Clave foránea para usuario
-                FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE SET NULL -- Clave foránea para cliente
+                FOREIGN KEY (user_id) REFERENCES users(id)
             )');
+
+
+            $this->prepare();
+            $this->insert("phones")->values([
+                "user_id" => 1,
+                "is_principal" => true,
+                "number_phone" => "7737-3234"
+            ]); 
+            $this->execute();
         });
     }
         

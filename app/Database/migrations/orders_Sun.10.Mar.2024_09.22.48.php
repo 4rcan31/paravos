5be4f4d8e12f54f67f3268006c9a5074c56e6b73
4rcan31 <rodrigo.francoescobar123@gmail.com>
2024-03-23 00:15:23 -0600

    Add time column, and more

--- app/Database/migrations/orders_Sun.10.Mar.2024_09.22.48.php
+++ app/Database/migrations/orders_Sun.10.Mar.2024_09.22.48.php
@@ -7,23 +7,25 @@ class orders extends Migration {
             $this->query('CREATE TABLE orders (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 product_id INT NOT NULL,
-                user_id INT, -- Podría ser nulo si es un cliente registrado
-                client_id INT, -- Podría ser nulo si es un cliente no registrado
-                email VARCHAR(255) NOT NULL,
-                address VARCHAR(255) NOT NULL,
+                user_id INT DEFAULT 1, -- Uno por que el cliente por default sera "1"
+                address_id INT,
+                reference TEXT DEFAULT NULL,
+                email VARCHAR(255),
                 phone VARCHAR(20) NOT NULL,
                 payment_status VARCHAR(20),
                 tracking_number VARCHAR(255),
                 shipping_date DATE,
                 delivery_date DATE,
+                delivery_time TIME,
                 notes TEXT DEFAULT NULL,
                 payment_method VARCHAR(50),
                 order_status VARCHAR(20),
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 FOREIGN KEY (product_id) REFERENCES products(id),
-                FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL, -- Clave foránea para usuario
-                FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE SET NULL -- Clave foránea para cliente
+                FOREIGN KEY (user_id) REFERENCES users(id), -- Clave foránea para 
+                FOREIGN KEY (address_id) REFERENCES addresses(id)
             )');
+            
             /* 
                 Tanto user_id como client_id pueden ser nulos.
 Si el pedido es realizado por un cliente registrado, entonces el user_id se asigna, mientras que el client_id se deja nulo.

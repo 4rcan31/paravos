8b764bed81081a637faa79d5e091365716903058
4rcan31 <rodrigo.francoescobar123@gmail.com>
2024-03-23 00:18:46 -0600

    Reference to partners, add update column, and insert some products for testing

--- app/Database/migrations/products_Sun.10.Mar.2024_09.22.00.php
+++ app/Database/migrations/products_Sun.10.Mar.2024_09.22.00.php
@@ -6,15 +6,18 @@ class products extends Migration {
         $this->create("products", function($table) {
             $this->query('CREATE TABLE products (
                 id INT PRIMARY KEY AUTO_INCREMENT,
+                category_id INT,
+                partners_id INT,
                 name VARCHAR(255) NOT NULL,
                 description_large TEXT,
                 description_short TEXT,
                 url_img TEXT,
                 stock INT DEFAULT 1,
                 price DECIMAL(10, 2) NOT NULL,
-                category_id INT,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (category_id) REFERENCES categories(id)
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                FOREIGN KEY (category_id) REFERENCES categories(id),
+                FOREIGN KEY (partners_id) REFERENCES partners(id)
             )');
 
             for ($i = 1; $i <= 4; $i++) { // suponiendo que las categorías tienen ids del 1 al 4
@@ -22,10 +25,12 @@ class products extends Migration {
                     $this->prepare();
                     $this->insert('products')->values([
                         'name' => "Nombre",
+                        'partners_id' => 1, //uno es el pather principal
                         'description_large' => "Descripción larga del producto $j",
                         'description_short' => "Descripción Corta del producto $j",
                         'url_img' => 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Rojos.png',
                         'price' => rand(10, 100), // Precio aleatorio entre 10 y 100
+                        'stock' => rand(10, 25),
                         'category_id' => $i,
                     ]);
                     $this->execute();

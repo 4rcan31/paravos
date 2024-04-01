069f93294aa25fb060bdbf3b15b2bbecc1f7e82e
4rcan31 <rodrigo.francoescobar123@gmail.com>
2024-03-31 21:13:47 -0600

    add datatables files scripts

--- app/Views/layouts/principal/scripts.php
+++ app/Views/layouts/principal/scripts.php
@@ -6,6 +6,10 @@ function scriptsPrincipal(){
   echo requiresStaticFiles([
     routePublic('vendor/jquery/jquery.min.js'),
     routePublic('vendor/bootstrap/js/bootstrap.bundle.min.js'),
+    routePublic('vendor/datatables/dataTables.js'),
+    routePublic('vendor/datatables/dataTables.bootstrap5.js'),
+    routePublic('vendor/datatables/dataTables.responsive.js'),
+    routePublic('vendor/datatables/responsive.bootstrap5.js'),
     routePublic("assets/js/store.js"),
     routePublic('vendor/owl/owl.js'),
     routePublic('vendor/slick/slick.js'),   

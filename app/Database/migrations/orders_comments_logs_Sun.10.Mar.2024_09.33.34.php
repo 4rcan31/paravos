d55e277e16fa3db4bc223e9afe8f90c958307bf9
4rcan31 <da.esfra12@gmail.com>
2024-08-20 01:26:11 -0600

    rename file, now commets can be global, staff to staff and client to staff

--- app/Database/migrations/orders_comments_logs_Sun.10.Mar.2024_09.33.34.php
+++ app/Database/migrations/orders_comments_logs_Sun.10.Mar.2024_09.33.34.php
@@ -3,15 +3,15 @@
 class orders_comments_by_staff extends Migration {
         
     public function up() {
-        $this->create("orders_comments_by_staff", function($table) {
-            $this->query('CREATE TABLE orders_comments_by_staff (
+        $this->create("orders_comments_logs", function($table) {
+            $this->query('CREATE TABLE orders_comments_logs (
                 id INT PRIMARY KEY AUTO_INCREMENT,
                 id_order INT,
-                id_staff INT,
-                priority VARCHAR(20),
-                comment_status VARCHAR(20),
+                id_staff INT DEFAULT NULL,
                 attachment VARCHAR(255) DEFAULT NULL,
-                private_note TEXT,
+                private_note TEXT, -- Esta nota es solamente para el staff
+                public_note TEXT, -- esta nota la puede ver el cliente y el staff
+                type VARCHAR(100), -- Aca se puede guardar el tipo de comentario, esto sera seteado por el sistema, como comentario de cancelacion, de info, de warn etc
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 FOREIGN KEY (id_order) REFERENCES orders(id),

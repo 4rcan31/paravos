c9e1267ea601ae151d18c9128cafbbc361af62eb
4rcan31 <da.esfra12@gmail.com>
2024-08-20 01:32:58 -0600

    Add columns for authenticated parthers view

--- app/Database/migrations/partners_Sat.23.Mar.2024_05.07.23.php
+++ app/Database/migrations/partners_Sat.23.Mar.2024_05.07.23.php
@@ -6,6 +6,10 @@ class partners extends Migration {
             $this->query('CREATE TABLE partners (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                user VARCHAR(255) NOT NULL,
+                password VARCHAR(255) NOT NULL,
+                remember_token VARCHAR(255),
                 description VARCHAR(255) NOT NULL,
                 img TEXT NOT NULL,
                 latitude TEXT NOT NULL,
@@ -13,11 +17,14 @@ class partners extends Migration {
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )');
-
+            import('Encrypt/hasher.php', false, '/core');
 
             $this->prepare();
             $this->insert('partners')->values([
                 'name' => "Para vos (Interno)",
+                'email' => 'paravos@gmail.com',
+                'user' => 'paravos',
+                'password' => Hasher::make('123'),
                 'description' => "Ecommers se productos variados",
                 "img" => "https://upload.wikimedia.org/wikipedia/commons/3/3f/Rojos.png",
                 "longitude" => "92.795435",
@@ -28,6 +35,9 @@ class partners extends Migration {
             $this->prepare();
             $this->insert('partners')->values([
                 'name' => "Clinica Medica Escobar",
+                'email' => 'clinicamendicaescobar@gmail.com',
+                'user' => 'clinicamedidaescobar',
+                'password' => Hasher::make('123'),
                 'description' => "Clinica, laboratorio y farmacia para diferentes pacientes, nos hubicamos en San Martin en frente del don pollo",
                 "img" => "https://upload.wikimedia.org/wikipedia/commons/3/3f/Rojos.png",
                 "longitude" => "92.795435",
